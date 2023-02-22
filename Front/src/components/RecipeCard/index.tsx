243d31e66a0f1fb1d602f5e475b78f765a413a3e
Dylan Stockinger <pro.stockinger@gmail.com>
2023-02-22 21:12:28 +0100

    🐛

--- Front/src/components/RecipeCard/index.tsx
+++ Front/src/components/RecipeCard/index.tsx
@@ -65,6 +65,61 @@ export const SliderComponent = (recipes: SliderComponentProps) => {
               alt=""
             />
             <p>{recipe.description}</p>
+            <div className="text-center mb-[5px] mt-[30px]">
+              <button
+                type="button"
+                className=" text-white bg-green-700 hover:bg-green-800 focus:outline-none  font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900"
+              >
+                Show the recipe
+              </button>
+            </div>
+          </div>
+        ))}
+      </Slider>
+
+      <h1 className="text-5xl mb-8">
+        {" "}
+        Les <span className="text-green-600">5</span> recettes les mieux notées
+        !
+      </h1>
+
+      <Slider {...settings}>
+        {dataRecipe.map((recipe) => (
+          <div className="  inline-flex rounded-md shadow-sm">
+            <h1 className="text-2xl">{recipe.title}</h1>
+            <img
+              className="w-full"
+              src={`/recipesImage/${recipe.picture}`}
+              alt=""
+            />
+            <p>{recipe.description}</p>
+            <div className="text-center mb-[10px] mt-[30px]">
+              <button
+                type="button"
+                className=" text-white bg-green-700 hover:bg-green-800 focus:outline-none  font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900"
+              >
+                Show the recipe
+              </button>
+            </div>
+          </div>
+        ))}
+      </Slider>
+      <h1 className="text-5xl mb-8">
+        {" "}
+        Les <span className="text-green-600">5</span> recettes les mieux notées
+        !
+      </h1>
+
+      <Slider {...settings}>
+        {dataRecipe.map((recipe) => (
+          <div className="  inline-flex rounded-md shadow-sm">
+            <h1 className="text-2xl">{recipe.title}</h1>
+            <img
+              className="w-full"
+              src={`/recipesImage/${recipe.picture}`}
+              alt=""
+            />
+            <p>{recipe.description}</p>
             <div className="text-center mb-[10px] mt-[30px]">
               <button
                 type="button"
